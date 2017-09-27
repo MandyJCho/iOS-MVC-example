80b6232f3816cec39f81589d7fb143a70305e233
MandyJCho <chomandy96@gmail.com>
2017-09-26 23:45:48 -0500

    Initial Commit

---
+++ iOS-MVC-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-MVC-example
+//
+//  Created by Mandy Cho on 9/26/17.
+//  Copyright © 2017 Canvas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
